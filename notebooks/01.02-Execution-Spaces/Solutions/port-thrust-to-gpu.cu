04e3160a04938de31a5e68d8a008e0f2b55f521e
Samuele Ruffino <samuele.ruffino@gmail.com>
2025-10-21 15:34:45 +0200

    removed all notebooks (probably I am not allowed)

--- notebooks/01.02-Execution-Spaces/Solutions/port-thrust-to-gpu.cu
+++
@@ -1,9 +0,0 @@
-#include "dli.h"
-
-int main() {
-  thrust::universal_vector<int> vec{1, 2, 3};
-  thrust::for_each(
-      thrust::device, vec.begin(), vec.end(), [] __device__(int val) {
-        std::printf("printing %d on %s\n", val, dli::execution_space());
-      });
-}
--- notebooks/01.02-Execution-Spaces/Sources/port-thrust-to-gpu.cu
+++
@@ -1,8 +0,0 @@
-#include "dli.h"
-
-int main() {
-    thrust::universal_vector<int> vec{ 1, 2, 3 };
-    thrust::for_each(thrust::host, vec.begin(), vec.end(), []__host__(int val) {
-        std::printf("printing %d on %s\n", val, dli::execution_space());
-    });
-}
